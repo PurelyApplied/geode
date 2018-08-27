f8638decd391dcdfd8cc5b458b080e60a7808c25
Robert Houghton <rhoughton@pivotal.io>
2018-08-27 15:22:26 -0700

    These also appear unused.

--- find_version_usages.sh
+++ find_version_usages.sh
@@ -7,7 +7,7 @@ echo ${BASH_SOURCE[0]}
 echo $(dirname ${BASH_SOURCE[0]})
 cd $(dirname ${BASH_SOURCE[0]})
 
-gradleFiles="$(find . -name "*.gradle" -a -type f)"
+gradleFiles="$(find . -name "*.gradle" -a -type f -a ! -name dependency-constraints.gradle)"
 for item in $(grep -Eo "\S*\.version" /Users/pivotal/workspace/geode/gradle/dependency-versions.properties) ; do
   echo ${item}
   grep "${item}" ${gradleFiles}
